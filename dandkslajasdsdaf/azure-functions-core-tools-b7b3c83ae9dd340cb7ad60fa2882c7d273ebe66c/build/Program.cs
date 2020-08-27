ca1e8f7cf4b435b8ea810c9653d6095e90a5d180
Roqibbd <70210954+Roqibbd@users.noreply.github.com>
2020-08-27 23:26:38 +0600

    Trying

---
+++ dandkslajasdsdaf/azure-functions-core-tools-b7b3c83ae9dd340cb7ad60fa2882c7d273ebe66c/build/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Linq;
+using System.Net;
+using static Build.BuildSteps;
+
+namespace Build
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            Orchestrator
+                .CreateForTarget(args)
+                .Then(TestSignedArtifacts, skip: !args.Contains("--signTest"))
+                .Then(Clean)
+                .Then(LogIntoAzure, skip: !args.Contains("--ci"))
+                .Then(RestorePackages)
+                .Then(ReplaceTelemetryInstrumentationKey, skip: !args.Contains("--ci"))
+                .Then(DotnetPublish)
+                .Then(FilterPowershellRuntimes)
+                .Then(AddDistLib)
+                .Then(AddTemplatesNupkgs)
+                .Then(AddTemplatesJson)
+                .Then(AddGoZip)
+                .Then(TestPreSignedArtifacts, skip: !args.Contains("--ci"))
+                .Then(CopyBinariesToSign, skip: !args.Contains("--ci"))
+                .Then(Test)
+                .Then(Zip)
+                .Then(UploadToStorage, skip: !args.Contains("--ci"))
+                .Run();
+        }
+    }
+}
